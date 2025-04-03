3a4418bea33d760229914155fa10ec77ed9640bb
MuhammadYahyo2112 <yahyo0121@gmail.com>
2025-04-03 14:54:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let  ismingigz = prompt("Ismingiz")
+let  familyangiz = prompt("Familyangiz")
+let  ochistvangiz = prompt("O'chistvangiz")
+let  yoshingiz = prompt("Yoshingiz")
+let  qaysiMaktabdaOqisiz = prompt("Qaysi Maktabda O'qisiz")
+let  nechanchisinifdaOqisiz = prompt("Nechanchisinifda O'qisiz")
+let  sizQandayTogaraklargaBorasiz = prompt("Siz Qanday Togaraklarga Borasiz")
+let  sizgaMarsniKimtaklifQilgan = prompt("Sizga Marsni Kimtaklif Qilgan")
+let  sizMarsdaNimaYonalishdaOqisiz = prompt("Siz Marsda Nima Yonalishda O'qisiz")
+let sizgaMarsYoqadimi  = prompt("Sizga Mars Yoqadimi")
+
+let Javob = alert("Sizning Isming'iz " + ismingigz + " Sizning Familyangiz " +  familyangiz + " Sizning O'chistvangiz " +  ochistvangiz 
+     + " Sizning Yoshingiz  " +  yoshingiz + " Sizning Maktabingiz  " +  qaysiMaktabdaOqisiz + " Sizning Sinifingiz  "  +  nechanchisinifdaOqisiz 
+    +  "  Sizning Togaraklaringiz    " +   sizQandayTogaraklargaBorasiz  +  "  Sizni Marsda O'qishingizni Kim Taklif Qilgan   " + sizgaMarsniKimtaklifQilgan 
+  + "  Siz Marsda Qanday Yonalishda O'qiysiz " +  sizMarsdaNimaYonalishdaOqisiz + "  Sizga Mars Yoqadimi " +  sizgaMarsYoqadimi )
+  
